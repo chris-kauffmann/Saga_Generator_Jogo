be53647e6e718024a1196331498c48798192bef5
chris-kauffmann <chris_kauffmann@hotmail.com>
2023-03-27 15:25:05 -0300

    Commit Chris

--- SagaGenerator/src/sagaGeneratorMain/Main.java
+++ SagaGenerator/src/sagaGeneratorMain/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner leia = new Scanner(System.in);
 		
 		
